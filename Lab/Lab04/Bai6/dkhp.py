19d7efeba20a0aba088dbddfedc0dfe2d2b00bc5
Dat0309 <anhtheboy011@gmail.com>
2022-09-22 16:37:29 +0700

    Update dkhp.py

--- Lab/Lab04/Bai6/dkhp.py
+++ Lab/Lab04/Bai6/dkhp.py
@@ -1,5 +1,31 @@
+from cmath import isnan
+import re
 from tkinter import *
+from tkinter import messagebox
 from tkinter.ttk import Combobox
+
+def onVaslid(id,name,dob,email,phone, course, year, sub):
+    if (checkEmail(email.get()) or checkDate(dob.get()) or len(str(id.get())) < 7 or id == 0 
+    or len(str(name.get())) == 0 or len(str(phone.get())) < 10 
+    or len(str(course.get())) == 0 or len(str(year.get())) == 0 or len(sub) == 0):
+        messagebox.showerror('Lỗi', 'Bạn chưa nhập đúng, đủ thông tin! vui lòng nhập lại')
+        return False
+    print(str(id.get()) + name.get() + email.get() + phone.get() + str(course.get()) + year.get() + str(len(sub)))
+    return True
+
+def checkEmail(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.fullmatch(regex, str(email))
+
+def focus1(event):
+    year_field.focus_set()
+
+def checkDate(date):
+    regex = "^[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{4}$"
+    return re.match(regex, str(date))
+    
+def submit(id, name, email,dob, phone, course, year, sub):
+    onVaslid(id, name, dob, email,phone, course, year, sub)
 
 if __name__ =="__main__":
     root = Tk()
@@ -8,14 +34,21 @@ if __name__ =="__main__":
     root.title('ĐĂNG KÝ HỌC PHẦN')
     root.geometry('800x500')
     
-    id = StringVar()
+    picks = ['Pyhton', 'Java', 'CNPM', 'Web']
+    
+    def only_numbers(char):
+        return char.isdigit()
+    
+    validation_id = root.register(only_numbers)
+    
+    id = IntVar(value=0000000)
     name = StringVar()
     dob = StringVar()
     email = StringVar()
     phone = StringVar()
-    course = StringVar()
+    course = IntVar(value=1)
     year = StringVar()
-    sub = StringVar()
+    sub = []
     
     title = Label(root, text='THÔNG TIN ĐĂNG KÝ HỌC PHẦN', fg='Red', bg='light green', font=("Arial", 20))
     
@@ -28,13 +61,25 @@ if __name__ =="__main__":
     year_lb = Label(root, text='Năm học', bg='light green')
     sub_lb = Label(root, text='Môn học', bg='light green')
     
-    id_field = Entry(root,width=60, font=('Arial 11'), textvariable=id)
+    id_field = Entry(root,width=60, font=('Arial 11'), textvariable=id, validate='key', validatecommand=(validation_id, '%s'))
     name_field = Entry(root,width=60, font=('Arial 11'), textvariable=name)
     dob_field = Entry(root,width=60, font=('Arial 11'), textvariable=dob)
     email_field = Entry(root,width=60, font=('Arial 11'), textvariable=email)
     phone_field = Entry(root,width=60, font=('Arial 11'), textvariable=phone)
     course_field = Entry(root,width=60, font=('Arial 11'), textvariable=course)
     year_field = Combobox(root,width=58, font=('Arial 11'), textvariable=year)
+    year_field['values'] = ('2022-2023', '2023-2024', '2024-2025')
+    year_field['state'] = 'readonly'
+    year_field.bind('<<ComboboxSelected>>', focus1)
+    
+    submit_btn = Button(root,text='Đăng Ký', command=lambda : submit(id, name, dob,email,phone,course,year, sub))
+    exit_btn = Button(root, text='Thoát', command=root.destroy)
+    
+    for pick in picks:
+        var = StringVar()
+        chk = Checkbutton(root, text=pick, variable=var)
+        chk.pack(side=LEFT, anchor='e', expand=YES)
+        sub.append(var.get())
     
     title.place(relx=0.5, rely=0.1,anchor=CENTER)
     id_lb.place(relx=0.1,rely=0.15)
@@ -53,5 +98,8 @@ if __name__ =="__main__":
     phone_field.place(relx=0.25,rely=0.35)
     course_field.place(relx=0.25,rely=0.4)
     year_field.place(relx=0.25,rely=0.45)
+    
+    submit_btn.place(relx=0.5, rely=0.8)
+    exit_btn.place(relx=0.6, rely=0.8)
 
     root.mainloop()
